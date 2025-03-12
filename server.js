64f7ec743ae593ef251f57b6ee9eba2c19192c45
Bruno's Laptop <brunorafael72@outlook.com>
2025-03-11 23:44:57 -0300

    Added Layout and Partials

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
 
 /* ***********************
- * Routes
+ * View Engine and Templates
  *************************/
+
+app.set("view engine","ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 app.use(static)
 
 /* ***********************
@@ -29,4 +34,8 @@ app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
 
-const expressLayouts = require("express-ejs-layouts")
+
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
