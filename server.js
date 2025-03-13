26c3d4886b376e1f944344ae63f68364ebcd7afd
Bruno's Laptop <brunorafael72@outlook.com>
2025-03-12 23:27:57 -0300

    Path fix to images

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.set("view engine","ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
 app.use(static)
+app.use(express.static('public'));
 
 /* ***********************
  * Local Server Information
